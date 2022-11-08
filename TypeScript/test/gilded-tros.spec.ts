003f52d89f577481eb99d1a471c3d7b7f1bcc701
Lennart Vandoorne <lennrtv@gmail.com>
2022-11-08 14:31:19 +0100

    Fix item foo unit test

--- TypeScript/test/gilded-tros.spec.ts
+++ TypeScript/test/gilded-tros.spec.ts
@@ -2,8 +2,13 @@ import {Item} from '../src/item';
 import {GildedTros} from '../src/gilded-tros';
 
 describe('GildedTrosTest', () => {
-    const items: Item[] = [new Item('foo', 0, 0)];
-    const app: GildedTros = new GildedTros(items);
-    app.updateQuality();
-    expect(app.items[0].name).toEqual('fixme');
+    it('should add item foo', () => {
+        const items: Item[] = [new Item('foo', 0, 0)];
+        const app: GildedTros = new GildedTros(items);
+        expect(app.items[0].name).toEqual('foo');
+        expect(app.items[0].sellIn).toEqual(0);
+        expect(app.items[0].quality).toEqual(0);
+        app.updateQuality();
+        expect(app.items[0].sellIn).toEqual(-1);
+    });
 });
