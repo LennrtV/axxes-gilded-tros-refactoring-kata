085241847ef0f529ad3c033dd1cbfa75dc0fd0d3
Lennart Vandoorne <lennrtv@gmail.com>
2022-11-09 08:05:29 +0100

    Add comment and bring capQuality function into GildedTros class

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -6,14 +6,17 @@ export class GildedTros {
 
     }
 
+    /**
+     * Updates the quality of all items in the inventory by name.
+     */
     public updateQuality(): void {
         this.items.forEach(item => {
             switch(item.name) {
                 case 'Good Wine':
                     if (item.sellIn > 0) {
-                        item.quality = capQuality(item.quality + 1);
+                        item.quality = this.capQuality(item.quality + 1);
                     } else {
-                        item.quality = capQuality(item.quality + 2);
+                        item.quality = this.capQuality(item.quality + 2);
                     }
                     item.sellIn = item.sellIn - 1;
                     break;
@@ -24,11 +27,11 @@ export class GildedTros {
                 case 'Backstage passes for Re:Factor':
                 case 'Backstage passes for HAXX':
                     if (item.sellIn >= 11) {
-                        item.quality = capQuality(item.quality + 1);
+                        item.quality = this.capQuality(item.quality + 1);
                     } else if (item.sellIn >=6 && item.sellIn < 11) {
-                        item.quality = capQuality(item.quality + 2);
+                        item.quality = this.capQuality(item.quality + 2);
                     } else if (item.sellIn > 0 && item.sellIn < 6) {
-                        item.quality = capQuality(item.quality + 3);
+                        item.quality = this.capQuality(item.quality + 3);
                     } else {
                         item.quality = 0;
                     }
@@ -39,18 +42,18 @@ export class GildedTros {
                 case 'Long Methods' :
                 case 'Ugly Variable Names':
                     if (item.sellIn > 0) {
-                        item.quality = capQuality(item.quality - 2);
+                        item.quality = this.capQuality(item.quality - 2);
                     } else {
-                        item.quality = capQuality(item.quality - 4);
+                        item.quality = this.capQuality(item.quality - 4);
                     }
                     item.sellIn = item.sellIn - 1;
                     break;
 
                 default:
                     if (item.sellIn > 0) {
-                        item.quality = capQuality(item.quality - 1);
+                        item.quality = this.capQuality(item.quality - 1);
                     } else {
-                        item.quality = capQuality(item.quality - 2);
+                        item.quality = this.capQuality(item.quality - 2);
                     }
                     item.sellIn = item.sellIn - 1;
                     break;
@@ -58,8 +61,12 @@ export class GildedTros {
         });
     }
 
-}
-
-export function capQuality(quality: number): number {
-    return Math.min(Math.max(quality, 0), 50);
+    /**
+     * Check and cap an item quality value to be at least 0 and at most 50.
+     * @param quality the number to cap
+     * @returns quality capped between 0 and 50
+     */
+    private capQuality(quality: number): number {
+        return Math.min(Math.max(quality, 0), 50);
+    }
 }
